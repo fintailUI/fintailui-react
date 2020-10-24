fb5e06ef5a7505fda1ee53951bcf5d484f37fc5d
Vinsensius Danny <vinsensius.danny@gmail.com>
2020-10-24 23:52:12 +0700

    Alert component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles/tailwind.output.css'
 
 export { default as Accordion } from './Accordion'
+export { default as Alert } from './Alert'
 export { default as Button } from './Button'
 export { default as Card } from './Card'
