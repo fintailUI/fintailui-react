c05a95e67b39af7e726fc64fe05ca747a42f0427
Vinsensius Danny <vinsensius.danny@gmail.com>
2020-10-25 00:43:40 +0700

    Badge component

--- .storybook/stories/Button/index.stories.jsx
+++ .storybook/stories/Button/index.stories.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Button } from '../../../dist';
 
 export default {
-  title: 'Buttons',
+  title: 'Button',
   component: Button,
 };
 
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './styles/tailwind.output.css'
 
 export { default as Accordion } from './Accordion'
 export { default as Alert } from './Alert'
+export { default as Badge } from './Badge'
 export { default as Button } from './Button'
 export { default as Card } from './Card'
